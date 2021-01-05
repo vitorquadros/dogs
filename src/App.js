eb4b9e08a7cfff20a5f4c7ed0aa7fdd9c6b7628f
Vitor Quadros <viitorquadros@hotmail.com>
2021-01-05 18:53:53 -0300

    feat: userContext
    creating a context for user functions to handle login

--- src/App.js
+++ src/App.js
@@ -5,17 +5,20 @@ import Footer from "./Components/Footer";
 import Home from "./Components/Home";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./Components/Login/Login";
+import { UserStorage } from "./UserContext";
 
 function App() {
   return (
     <div>
       <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login/*" element={<Login />} />
-        </Routes>
-        <Footer />
+        <UserStorage>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login/*" element={<Login />} />
+          </Routes>
+          <Footer />
+        </UserStorage>
       </BrowserRouter>
     </div>
   );
